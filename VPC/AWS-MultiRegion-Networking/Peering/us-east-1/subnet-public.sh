cfabc50f720ad02f0e778ea033756f00b521f57d
Arbz-N <arbaznaeem555@gmail.com>
2025-12-07 22:51:35 +0500

    Update subnet-public.sh

--- VPC/AWS-MultiRegion-Networking/Peering/us-east-1/subnet-public.sh
+++ VPC/AWS-MultiRegion-Networking/Peering/us-east-1/subnet-public.sh
@@ -8,4 +8,9 @@ aws ec2 create-subnet \
   --availability-zone us-east-1a \
   --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=YourPublicSubnetUSA}]'
 
-echo "Public Subnet Created. Replace <your-vpc-id>."
+# ENABLE AUTO-ASSIGN PUBLIC IP ON SUBNET (USA)
+
+aws ec2 modify-subnet-attribute \
+  --subnet-id <your-subnet-id> \     # Replace with your Subnet ID
+  --map-public-ip-on-launch
+
