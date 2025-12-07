dae21713fbc0d65da33aced59c0498556e018270
Arbz-N <arbaznaeem555@gmail.com>
2025-12-07 22:39:27 +0500

    Update subnet-public.sh

--- VPC/AWS-MultiRegion-Networking/Peering/eu-west-2/subnet-public.sh
+++ VPC/AWS-MultiRegion-Networking/Peering/eu-west-2/subnet-public.sh
@@ -7,3 +7,12 @@ aws ec2 create-subnet \
   --cidr-block 10.0.0.0/24 \
   --availability-zone eu-west-2a \
   --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=YourPublicSubnetEU}]'
+
+
+# ENABLE AUTO-ASSIGN PUBLIC IP ON SUBNET 
+
+aws ec2 modify-subnet-attribute \
+  --subnet-id <your-subnet-id> \     # Replace with your Subnet ID
+  --map-public-ip-on-launch
+
+echo "Auto-assign Public IP enabled for Subnet: <your-subnet-id>"
