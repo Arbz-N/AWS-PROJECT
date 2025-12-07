ad4d120af9e8d923a6ea1679e247f384c9a11898
Arbz-N <arbaznaeem555@gmail.com>
2025-12-07 22:07:56 +0500

    Delete VPC/AWS-MultiRegion-Networking/us-east-1 directory

--- VPC/AWS-MultiRegion-Networking/us-east-1/route-table-public.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# CREATE PUBLIC ROUTE TABLE (USA)
-
-aws ec2 create-route-table \
-  --vpc-id <your-vpc-id> \
-  --tag-specifications 'ResourceType=route-table,Tags=[{Key=Name,Value=YourPublicRTUSA}]'
-
-echo "Public Route Table Created."
