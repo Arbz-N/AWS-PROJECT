a781423fdd64201d0d25f8c48295b0fe2463f8f9
Arbz-N <arbaznaeem555@gmail.com>
2025-12-07 22:10:11 +0500

    Delete VPC/AWS-MultiRegion-Networking/eu-west-2 directory

--- VPC/AWS-MultiRegion-Networking/eu-west-2/vpc-create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# CREATE VPC (EUROPE Region - eu-west-2)
-
-aws ec2 create-vpc \
-  --cidr-block 192.0.0.0/16 \
-  --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=YourVpcNameEU}]'
